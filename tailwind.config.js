7c2bc4d7f07e38c778aabb7cb73b892593efb8a9
Diyor Khaydarov <diyor28july@gmail.com>
2025-04-12 13:23:19 +0500

    chore: keep css files up to date

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,11 +29,21 @@ module.exports = {
       borderColor: {
         primary: "oklch(var(--clr-border-primary))",
         secondary: "oklch(var(--clr-border-secondary))",
+        green: "oklch(var(--clr-border-green))",
+        pink: "oklch(var(--clr-border-pink))",
+        yellow: "oklch(var(--clr-border-yellow))",
+        blue: "oklch(var(--clr-border-blue))",
+        purple: "oklch(var(--clr-border-purple))",
       },
       colors: {
         100: "oklch(var(--clr-text-100))",
         200: "oklch(var(--clr-text-200))",
         300: "oklch(var(--clr-text-300))",
+        green: "oklch(var(--clr-text-green))",
+        pink: "oklch(var(--clr-text-pink))",
+        yellow: "oklch(var(--clr-text-yellow))",
+        blue: "oklch(var(--clr-text-blue))",
+        purple: "oklch(var(--clr-text-purple))",
         avatar: "oklch(var(--clr-avatar-text))",
         black: {
           DEFAULT: "oklch(var(--black))",
@@ -55,6 +65,20 @@ module.exports = {
           100: "oklch(var(--red-100))",
           200: "oklch(var(--red-200))",
           500: "oklch(var(--red-500) / <alpha-value>)",
+        },
+        badge: {
+          pink: "oklch(var(--clr-badge-pink))",
+          yellow: "oklch(var(--clr-badge-yellow))",
+          green: "oklch(var(--clr-badge-green))",
+          blue: "oklch(var(--clr-badge-blue))",
+          purple: "oklch(var(--clr-badge-purple))",
+          gray: "oklch(var(--clr-badge-gray))",
+        },
+        success: {
+          DEFAULT: "oklch(var(--green-500) / <alpha-value>)"
+        },
+        on: {
+          success: "oklch(var(--white))"
         }
       },
     },
